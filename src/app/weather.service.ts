f8ad233660cc43e7abf5e24e4e04356da502c4a1
Anita <codingwithanita@gmail.com>
2020-08-10 18:06:53 -0700

    Service and Interface

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ICurrentWeatherData} from './icurrent-weather-data';
+import { environment } from 'src/environments/environment';
+import { ICurrentWeather } from './icurrent-weather';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getCurrentWeather (city: string, country: string){ 
+    return this.httpClient.get<ICurrentWeatherData>(`http://api.openweathermap.org/data/2.5/weather?q=${city}${country}&appid=${environment.appId}`)
+  }
+
+  transformToICurrentWeather(data:ICurrentWeatherData):ICurrentWeather {
+    return {
+      city: data.name,
+      country: data.sys.country,
+      description: data.weather[0].description,
+      temperature: data.main.temp,
+      image: `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`,
+      date: new Date(data.dt * 1000)
+    }
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: '2767f0d88f147414b8310313e47fc0a9'
 };
 
 /*
